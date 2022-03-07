b565e93a026c2506952df191ab13a1b2f6537931
Gazee1112 <muksinsaja12@gmail.com>
2022-03-07 23:17:53 +0700

    Mengubah file, udah hampir selesai

--- Tugas Praktikum/tugasPraktikum1/tugas.java
+++ Tugas Praktikum/tugasPraktikum1/tugas.java
@@ -1,53 +1,31 @@
 package tugasPraktikum1;
 import java.util.*;
 
-class Kategori{
-    private Buku book[];
+class c_Kategori{
     private String namaKategori;
-    
-    Kategori(String nama){
+
+    c_Kategori(String nama){
+        namaKategori = nama;
+    }
+    void setNamaK(String nama){
         this.namaKategori = nama;
     }
-    void setBook(Buku book[]){
-        this.book = book;
-    }
-    void setNama(String nama){
-        this.namaKategori = nama;
-    }
-    Buku[] getBuku(){
-        return this.book;
-    }
-    Buku getBuku (int indek){
-        return this.book[indek];
-    }
-    String getNamaKategori(){
+    String getNamaK(){
         return this.namaKategori;
     }
-
 }
-class Buku{
+
+class c_Buku{
     private String namaBuku;
     private String sinopsis;
-    private Penulis penulis[];
 
-    Buku (String nama, String sinopsis){
+    // Buku
+    c_Buku (String nama, String sinopsis){
         this.namaBuku = nama;
         this.sinopsis = sinopsis;
     }
-    void setPenulis(Penulis penulis[]){
-        this.penulis = penulis;
-    }
-    void setNama(String nama){
-        this.namaBuku = nama;
-    }
-    String getNama(){
+    String getNamaB(){
         return this.namaBuku;
-    }
-    Penulis[]getPenulis(){
-        return this.penulis;
-    }
-    Penulis getPenulis(int indek){
-        return this.penulis[indek];
     }
     String getSinopsis(){
         return this.sinopsis;
@@ -55,75 +33,99 @@ class Buku{
     int getSinopsisLength(){
         return sinopsis.length();
     }
-    
 }
 
-class Penulis{
-    private String namaPenulis;
+class c_Penulis{
+    private String namaPenulis1;
+    private String namaPenulis2;
 
-    Penulis(){
+    c_Penulis (String penulis1, String penulis2){
+        this.namaPenulis1 = penulis1;
+        this.namaPenulis2 = penulis2;
     }
 
-    Penulis(String nama){
-        this.namaPenulis = nama;
+    String getNamaP1(){
+        return this.namaPenulis1;
     }
-    void setNamaPenulis(String nama){
-        this.namaPenulis = nama;
+
+    String getNamaP2(){
+        return this.namaPenulis2;
     }
-    String getNamaPenulis(){
-        return this.namaPenulis;
-    }
+
 }
-
-class tugas{
+public class tugas {
     public static Scanner input = new Scanner(System.in);
-    public static Kategori kategori[] = new Kategori[3];
+    
+    // Pembuatan Array kategori, Buku, Penulis
+    public static c_Kategori kategori[] = new c_Kategori[3];
+    public static c_Buku buku[] = new c_Buku[6];
+    public static c_Penulis penulis[] = new c_Penulis[6];
+
     public static void main(String[] args) {
-        System.out.println("Selamat datang di aplikasi perpustakaan UB" + 
-                         "\n1. Memasukan Kategori, Buku, dan Penulisnya" +
-                         "\n2. Melihat isi kategori yang telah dibuat" +
-                         "\n3. Keluar Aplikasi");
-        int pilih = input.nextInt();input.nextLine();
-        
-        boolean kondisi = true;
-        while(kondisi){
+        int pilih;
+        do {
+            System.out.println("Selamat datang di aplikasi perpustakaan UB" + 
+                             "\n1. Memasukan Kategori, Buku, dan Penulisnya" +
+                             "\n2. Melihat isi kategori yang telah dibuat" +
+                             "\n3. Keluar Aplikasi");
+            System.out.print("Masukan pilihan : "); 
+            pilih = input.nextInt();input.nextLine();
             switch(pilih){
                 case 1 :
-                    indek();
+                    indeks();
                     break;
                 case 2 :
-                    cetak();
+                    print();
                     break;
                 case 3 : 
                     System.out.println("Terimakasih telah menggunakan aplikasi perpustakaan UB");
                     break;
+                } 
+        } while (pilih != 3);
+    }
+    
+    public static void indeks(){
+        int x = 0; // untuk membantu dalam penempatan array buku, sinopsis, dan penulis
+
+        for (int i = 0; i < kategori.length; i++){
+            System.out.print("Masukan nama kategori ke-" + (i + 1) + " : ");
+            kategori[i] = new c_Kategori(input.nextLine());
+             
+            for (int j = 0; j < 2; j++){ 
+                System.out.print("Masukan nama buku ke-" + (j + 1) + " : ");
+                String nama = input.nextLine();
+                System.out.print("Masukan sinopsis buku ke-" + (j + 1) + " : ");
+                String sinopsis = input.nextLine();
+                buku[x] = new c_Buku(nama, sinopsis);
+                
+                System.out.print("Masukan penulis ke-1 : ");
+                String penulis1 = input.nextLine();
+                System.out.print("Masukan penulis ke-2 : ");
+                String penulis2 = input.nextLine();
+                penulis[x] = new c_Penulis(penulis1, penulis2);
+                
+                x += 1;
             }
-        }
+        }          
     }
 
-    public static void indek(){
-        for (int i = 0; i < kategori.length; i++){
-            System.out.println("Masukan nama kategori ke-" + (i + 1) + " : ");
-            kategori[i] = new Kategori(input.nextLine());
-            // Buku[] buku = new Buku[2];
-            // for (int j = 0; j < buku.length; j++){
-            //     System.out.println("Masukan nama buku ke-" + (j + 1) + " : ");
-            //     String nama = input.next();
-            //     System.out.println("Masukan sinopsis buku ke-" + (j + 1) + " : ");
-            //     String penulis = input.next();
-            //     buku[j] = new Buku (nama,penulis);
-            //     Penulis penuliss[] = new Penulis [2];
-            //     for (int k = 0; k < penuliss.length; k++){
-            //         penuliss[k] = new Penulis();
-            //     }
-            // }
-        }
-    }
+    public static void print(){
+        int x = 0; // untuk membantu dalam mengakses array buku sinopsis, dan penulis
 
-    public static void cetak(){
-        for (int i = 0; i < kategori.length; i++){
-            System.out.println("Kategori ke " + (i + 1) + "adalah : " + kategori[i].getBuku());  
+        for (int i = 0; i < kategori.length;i++){
+            System.out.println("Kategori ke-" + (i + 1) + " adalah : " + kategori[i].getNamaK());  
+            for (int j = 0; j < 2; j++){
+                System.out.println("Buku ke-" + (j + 1) + " untuk kategori tersebut adalah" + buku[x].getNamaB());
+                System.out.println("Sinopsis : " + buku[x].getSinopsis()); 
+                System.out.println("Panjang perhuruf Sinopsis : " + buku[x].getSinopsisLength());
+                System.out.println("Penulis ke 1 untuk buku tersebut adalah : " + penulis[x].getNamaP1());
+                System.out.println("Penulis ke 2 untuk buku tersebut adalah : " + penulis[x].getNamaP2());   
+                x += 1;
+            }
             
+            System.out.println("");
+            System.out.println("==================================================================================");
+            System.out.println("");
         }
     }
 }
